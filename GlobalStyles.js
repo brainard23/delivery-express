d3349fb250200cc69b98b8dbdd4811ebb5ecf4d6
brainard <brainard.ordona@fligno.com>
2023-01-18 16:38:02 +0800

    app header update

--- GlobalStyles.js
+++ GlobalStyles.js
@@ -1,7 +1,6 @@
 import { StyleSheet, Platform } from 'react-native';
 export default StyleSheet.create({
     droidSafeArea: {
-        flex: 1,
         backgroundColor: "white",
         paddingTop: Platform.OS === 'android' ? 50 : 0
     },
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,28 +1,49 @@
-import { View, Text, SafeAreaView, Image } from "react-native";
+import { View, Text, SafeAreaView, Image, TextInput } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import GlobalStyles from "../GlobalStyles";
+import {
+  UserIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+  ChevronDownIcon,
+} from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
   useLayoutEffect(() => {
     navigation.setOptions({
-        headerShown: false
-    })
-  },[]);
+      headerShown: false,
+    });
+  }, []);
 
   return (
-    <SafeAreaView style={GlobalStyles.droidSafeArea}>
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
-            <Image 
-                source={require('../assets/images/adaptive-icon.png')}
-                className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-            />
-            <View>
-                <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-                <Text className="font-bold text-xl">Current Location</Text>
-            </View>
+    <SafeAreaView style={GlobalStyles.droidSafeArea} className="bg-white pt-5">
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 justify-between">
+        <Image
+          source={require("../assets/images/adaptive-icon.png")}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location <ChevronDownIcon size={20} color="#000000" />
+          </Text>
         </View>
+        <UserIcon size={35} color="#000000" />
+      </View>
+
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="gray" size={25} />
+          <TextInput
+            placeholder="Look for Restaurants"
+            keyboardType="default"
+          />
+        </View>
+
+        <AdjustmentsVerticalIcon color="#000000" />
+      </View>
     </SafeAreaView>
   );
 };
